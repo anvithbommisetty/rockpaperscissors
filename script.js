2b008dff5d874cf68adc206d4d36913ed737f161
anvithbommisetty <120590176+anvithbommisetty@users.noreply.github.com>
2023-06-04 18:22:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,225 @@
+const popup = document.querySelector('.popup');
+const container = document.querySelector('.container');
+const endpopup = document.querySelector('.endpopup');
+const xe = document.querySelector('.xe');
+const roundno = document.querySelector(".roundno");
+const computer = document.querySelector(".computer img");
+const player = document.querySelector(".player img");
+const computerPoints = document.querySelector(".computerPoints");
+const playerPoints = document.querySelector(".playerPoints");
+const options = document.querySelectorAll(".options button");
+const reset = document.querySelector(".reset");
+const instantres = document.querySelector('.instantres');
+const finalres = document.querySelector('.finalres');
+const form = document.querySelector("form");
+
+let rr = 0;
+
+window.addEventListener('load', () => {
+	popup.classList.add('showPopup');
+	popup.childNodes[1].classList.add('showPopup');
+	container.style.filter = 'blur(25px)';
+});
+
+form.addEventListener("submit", (event) => {
+	const data = new FormData(form);
+	for (const entry of data) {
+		rr = parseInt(entry[1]);
+	}
+	if (rr == 0) {
+		alert("Please select an option");
+		return;
+	}
+	event.preventDefault();
+	popup.classList.remove('showPopup');
+	popup.childNodes[1].classList.remove('showPopup');
+	container.style.filter = 'none';
+	displayScores();
+},
+	false
+);
+
+xe.addEventListener('click', () => {
+	var confirmed = window.confirm('Are you sure you want to exit?');
+
+	// Close the window if the user confirms
+	if (confirmed) {
+		let computer = parseInt(computerPoints.innerHTML);
+		let player = parseInt(playerPoints.innerHTML);
+		saveScore(player, computer);
+		window.close();
+	}
+});
+
+let round = 1;
+let games = 0;
+function disableAlloptions() {
+	for (let i = 0; i < options.length; i++) {
+		options[i].disabled = true;
+	}
+}
+function enableAlloptions() {
+	for (let i = 0; i < options.length; i++) {
+		options[i].disabled = false;
+	}
+}
+options.forEach((option) => {
+	option.addEventListener("click", () => {
+		computer.classList.add("shakeComputer");
+		player.classList.add("shakePlayer");
+		disableAlloptions();
+		games++;
+		setTimeout(() => {
+			computer.classList.remove("shakeComputer");
+			player.classList.remove("shakePlayer");
+
+
+			player.src = "./images/" + option.innerHTML + "Player.png"
+
+			const choice = ["STONE", "PAPER", "SCISSORS"];
+			let arrayNo = Math.floor(Math.random() * 3);
+			let computerChoice = choice[arrayNo];
+			computer.src = "./images/" + computerChoice + "Computer.png";
+
+			let cPoints = parseInt(computerPoints.innerHTML);
+			let pPoints = parseInt(playerPoints.innerHTML);
+
+			if (option.innerHTML === "STONE") {
+				if (computerChoice === "PAPER") {
+					computerPoints.innerHTML = cPoints + 1;
+					instantres.innerHTML = "Computer Won";
+				}
+				else if (computerChoice === "SCISSORS") {
+					playerPoints.innerHTML = pPoints + 1;
+					instantres.innerHTML = "Player Won";
+				}
+				else {
+					instantres.innerHTML = "Tie!!!";
+				}
+			} else if (option.innerHTML === "PAPER") {
+				if (computerChoice === "SCISSORS") {
+					computerPoints.innerHTML = cPoints + 1;
+					instantres.innerHTML = "Computer Won";
+				}
+				else if (computerChoice === "STONE") {
+					playerPoints.innerHTML = pPoints + 1;
+					instantres.innerHTML = "Player Won";
+				}
+				else {
+					instantres.innerHTML = "Tie!!!";
+				}
+			} else {
+				if (computerChoice === "STONE") {
+					computerPoints.innerHTML = cPoints + 1;
+					instantres.innerHTML = "Computer Won";
+				}
+				else if (computerChoice === "PAPER") {
+					playerPoints.innerHTML = pPoints + 1;
+					instantres.innerHTML = "Player Won";
+				}
+				else {
+					instantres.innerHTML = "Tie!!!";
+				}
+			}
+			instantres.style.visibility = "visible";
+		}, 850);
+
+		setTimeout(() => {
+			let cPoints = parseInt(computerPoints.innerHTML);
+			let pPoints = parseInt(playerPoints.innerHTML);
+
+			let win = "YOU WIN!!";
+			let lose = "YOU LOST";
+			let draw = "DRAW..."
+			if (games == rr) {
+				if (cPoints > pPoints) {
+					finalres.innerHTML = lose;
+					computerPoints.classList.add('.win');
+					playerPoints.classList.add('.lose');
+				}
+
+				else if (cPoints < pPoints) {
+					finalres.innerHTML = win;
+					computerPoints.classList.add('.lose');
+					playerPoints.classList.add('.win');
+				}
+				else finalres.innerHTML = draw;
+			}
+		}, 900)
+
+		setTimeout(() => {
+			enableAlloptions();
+			if (games == rr) {
+				let computer = parseInt(computerPoints.innerHTML);
+				let player = parseInt(playerPoints.innerHTML);
+				saveScore(player, computer);
+				disableAlloptions();
+				endpopup.classList.add('showPopup');
+				endpopup.childNodes[1].classList.add('showPopup');
+				container.style.filter = 'blur(25px)';
+			}
+			player.src = "./images/STONEPlayer.png";
+			computer.src = "./images/STONEComputer.png";
+			if (round < rr)
+				round++;
+			roundno.innerHTML = round;
+			instantres.style.visibility = "hidden";
+			reset.addEventListener("click", () => {
+				location.reload();
+			});
+		}, 2000);
+	});
+});
+
+function saveScore(player, computer) {
+	console.log(player, computer);
+	rr = rr / 5 - 1;
+
+	let scores = localStorage.getItem('rpsScore');
+	let temp = [
+		{
+			game: 0,
+			player: 0,
+			computer: 0
+		},
+		{
+			game: 1,
+			player: 0,
+			computer: 0
+		},
+		{
+			game: 2,
+			player: 0,
+			computer: 0
+		}
+	];
+	scores = scores ? JSON.parse(scores) : temp;
+
+	if (player >= computer) {
+		if (player > scores[rr].player) {
+			scores[rr].player = player;
+			scores[rr].computer = computer;
+		} else if (player == scores[rr].player) {
+			scores[rr].player = player;
+			scores[rr].computer = Math.min(computer, scores[rr].computer);
+		}
+	}
+
+	localStorage.setItem('rpsScore', JSON.stringify(scores));
+}
+
+function displayScores() {
+	let scoresContainer = document.getElementById('BestScore');
+	let idx = (rr / 5) - 1;
+
+	let scores = localStorage.getItem('rpsScore');
+
+	scores = scores ? JSON.parse(scores) : [];
+	if (!scores) {
+		scoresContainer.innerHTML = `BestScore:<span class='computerScore'>0</span>/<span class="playerScore">0</span>`;
+	} else {
+		scoresContainer.innerHTML = `BestScore:
+		<span class='computerScore'>${scores[idx].computer}</span>/
+		<span class="playerScore">${scores[idx].player}</span>`;
+	}
+}
